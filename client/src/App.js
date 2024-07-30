eedb07c852192a535c0ed65503fff18fc048dea6
Suraj <suraj1279.be21@chitkarauniversity.edu.in>
2024-07-31 00:09:42 +0530

    done

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Homepage';
 import CalculateCalory from './pages/Calculate-calories/Main';
+
 import './App.css';
 
 function App() {
